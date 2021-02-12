a9734e4972a566ebdbf87cea4202b0d2a3e48baa
GBethani <jinkiller69@gmail.com>
2021-02-12 21:12:54 +0530

    static files,templates,login form,login view,login url added

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from .forms import LoginForm
 # Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,username=cd['username'],password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request,user=user)
+                    return HttpResponse("Login Successful!")
+                else:
+                    return HttpResponse("Disabled Account")
+            else:
+                return HttpResponse("Invalid Login!")
+    else:
+        form = LoginForm()
+    context = {
+        'form':form
+    }
+    return render(request,'account/login.html',context=context)
