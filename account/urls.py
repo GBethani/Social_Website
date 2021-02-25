8181d462cc5cf97d6a9e73afcb2429ec7071931a
GBethani <jinkiller69@gmail.com>
2021-02-25 12:06:08 +0530

    login,logout views,dashboard added

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import user_login
+from django.contrib.auth import views as auth_views
+from .views import user_login, dashboard
 
 urlpatterns = [
-    path('',user_login,name='login'),
+    # path('',user_login,name='login'),
+    path('',auth_views.LoginView.as_view(),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 # Create your views here.
 def user_login(request):
@@ -23,3 +24,7 @@ def user_login(request):
         'form':form
     }
     return render(request,'account/login.html',context=context)
+
+@login_required
+def dashboard(request):
+    return render(request,'account/dashboard.html',{'section':'dashboard'})
